717d85c2b5aef54d8876c4e7b554b85563251370
Mercedes Tejada <mercedestejadaporcel@gmail.com>
2024-04-09 15:44:15 +0200

    first login styles

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,20 +4,19 @@ import bkImg from '../assets/img/test.jpg';
 
 const Login = () => {
     return (
-        <section>
-            <div>
-                <div>
-                    <form>
-                        <label className="w-80 h-10 p-2 m-2 text-lg font-bold text-gray-700">Iniciar sesión</label>
-                        <input type="email" placeholder="Correo electrónico" className="block w-80 h-10 p-2 m-2 border-2 border-gray-300 rounded-md" />
-                        <label className="w-80 h-10 p-2 m-2 text-lg font-bold text-gray-700">Contraseña</label>
-                        <input type="password" placeholder="Contraseña" className="block w-80 h-10 p-2 m-2 border-2 border-gray-300 rounded-md" />
-                        <button type="submit" className="block w-80 h-10 p-2 m-2 bg-gradient-to-r from-crayola via-pinkk to-blue-500 text-white font-bold rounded-md">Iniciar sesión</button>
-                    </form>
-                    <buttonc type="submit" className="block w-80 h-10 p-2 m-2 text-gray rounded-md text-center">Registrarse</buttonc>
-                </div>
+        <section  style={{backgroundImage: `url(${bkImg})`}} className='  bg-cover bg-center bg-no-repeat font-poppins font-light h-fit'>
+            <div className=" flex flex-col items-start justify-center">
+                <h1 className="p-4 text-4xl text-centerd">Start your <span className=' font-extrabold font-playfair text-6xl '>literature adventure</span></h1>
+                
+                    <div class = "bg-white shadow-gray-500 drop-shadow-md shadow-md rounded-3xl   p-10 w-fit h-fit flex flex-col items-start">
+                        <form className='flex flex-col gap-2'>
+                            <input type="email" placeholder="Your mail" className="block w-60  h-10 m-2 border-1 bg-gray-100 rounded-xl p-4" />
+                            <input type="password" placeholder="Password" className="block w-60  h-10 m-2 bg-gray-100 rounded-xl p-4" />
+                            <button type="submit" className="block w-60 h-10 p-2 m-2 button gradient rounded-3xl">Log in</button>
+                        </form>
+                        <a className=" h-10 w-60 p-2 m-2 text-gray rounded-md text-center" href='#'>Don't have an account? <span class= "text-blue-500">Sign up here.</span></a>
+                    </div>
             </div>
-            <img src={bkImg} alt="background" className="object-cover w-full h-screen" />
         </section>
            
     );
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,22 @@ export default {
   theme: {
     extend: {
       colors: {
-        crayola: "#E57238",
-        cornblue: "#9C77C9",
-        realemerald: "#66B67E",
-        pinkk: "#D37C90"
+        crayola: "#f15c31",
+        aqua: "#01A791",
+        sun: "#98a250",
+        crayola2: "#f0933e",
+        mutedpink: "#DE5E7C",
+        lavender: "#4390d0",
+        sunrise: "#ee902d",
+        salmon: "#e24c57"
+        /*crayola: "#E1733D",
+        lavender: "#7684BC",
+        mutedpink: "#DE5E7C"
+        */
+      },
+      fontFamily: {
+        'playfair': ['Playfair Display', 'serif'],
+        'poppins': ['Poppins', 'sans-serif']
       },
     },
   },
