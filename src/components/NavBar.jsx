981c4dfeccf4af9bc6423b8d3b5284c50c6f86c1
Mercedes Tejada <mercedestejadaporcel@gmail.com>
2024-04-10 17:20:12 +0200

    nav change

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,26 +10,27 @@ const NavBar = ({ isLoggedIn }) => {
 
 
     return (
-        <nav class="bg-white sticky top-0 p-4 border-b border-gray-200 z-10">
-            <div class="max-w-screen-xl mx-auto px-4 justify-between flex items-center py-3">
-                <a href="#" class="flex items-center space-x-2">
-                    <img src={logoImg} class="h-8" alt="Flowbite Logo" />
+        <nav className="bg-white sticky top-0 p-4 border-b border-gray-200 z-10">
+            <div className="max-w-screen-xl mx-auto px-4 flex justify-between items-center py-3">
+                <a href="#" className="flex items-center space-x-2">
+                    <img src={logoImg} className="h-8" alt="Flowbite Logo" />
                 </a>
-                {isLoggedIn ?
-                <div class="flex items-center space-x-4">
-                    <a href="#" class="text-gray-800 hover:text-gray-900 dark:text-white dark:hover:text-gray-300">Home</a>
-                    <a href="#" class="text-gray-800 hover:text-gray-900 dark:text-white dark:hover:text-gray-300">About</a>
-                    <a href="#" class="text-gray-800 hover:text-gray-900 dark:text-white dark:hover:text-gray-300">Services</a>
-                    <a href="#" class="text-gray-800 hover:text-gray-900 dark:text-white dark:hover:text-gray-300">Contact</a>
-                </div> : null}
-                <div>
-                    {isLoggedIn ? 
-                    <img src={auth.currentUser.photoURL} class="h-8 w-8 rounded-full" alt="User" onClick={() => signOut(auth)} />
-                    : null}
-
-                </div>
+                {isLoggedIn && (
+                    <div className="flex items-center space-x-4">
+                        <a href="#" className="text-gray-800 hover:text-gray-900 dark:text-white dark:hover:text-gray-300">Home</a>
+                        <a href="#" className="text-gray-800 hover:text-gray-900 dark:text-white dark:hover:text-gray-300">About</a>
+                        <a href="#" className="text-gray-800 hover:text-gray-900 dark:text-white dark:hover:text-gray-300">Services</a>
+                        <a href="#" className="text-gray-800 hover:text-gray-900 dark:text-white dark:hover:text-gray-300">Contact</a>
+                    </div>
+                )}
+                {isLoggedIn && (
+                    <div>
+                        <img src={auth.currentUser.photoURL} className="h-8 w-8 rounded-full cursor-pointer" alt="User" onClick={() => signOut(auth)} />
+                    </div>
+                )}
             </div>
         </nav>
+
     )
 }
 
