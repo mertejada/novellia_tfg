491945737449fd0e546b25223fdeeeac4236a7e8
Mercedes Tejada <mercedestejadaporcel@gmail.com>
2024-04-17 18:30:08 +0200

    user info registration

--- src/components/StepGoals.jsx
+++ src/components/StepGoals.jsx
@@ -1,33 +1,76 @@
 import React from "react";
 
-const StepsGoals = () => {
+const StepsGoals = ({ formData, setFormData }) => {
     return (
-            <div className="flex flex-col gap-4 items-center justify-center flex-wrap">
-                <h2 className="text-2xl text-center mb-6 text-gray-700">What are your reading goals for the year?<span className='text-black'>.</span></h2>
-                <div className="flex flex-col md:flex-row gap-7">
-                    <div className="flex flex-col w-full md:w-1/3 gap-2 items-center">
-                        <label className="text-gray-700 flex items-center" >
-                            📚
-                            Books per year
-                        </label>
-                        <input type="number" placeholder="10" className="h-12 p-4 rounded-3xl  border-gray-300 bg-gray-50 focus:outline-none font-light placeholder-slate-300" />
-                    </div>
-                    <div className="flex flex-col w-full md:w-1/3 gap-2 items-center">
-                        <label className="text-gray-700 flex items-center">
-                            📅
-                            Pages per month
-                        </label>
-                        <input type="number" placeholder="3000" className="h-12 p-4 rounded-3xl  border-gray-300 bg-gray-50 focus:outline-none font-light placeholder-slate-300" />
-                    </div>
-                    <div className="flex flex-col w-full md:w-1/3 gap-2 items-center">
-                        <label className="text-gray-700 flex items-center">
-                            🏷️
-                            Different genres
-                        </label>
-                        <input type="text" placeholder="3" className="h-12 p-4 rounded-3xl  border-gray-300 bg-gray-50 focus:outline-none font-light placeholder-slate-300" />
-                    </div>
+        <div className="flex flex-col gap-4 m-10 items-center justify-center flex-wrap">
+            <h2 className="text-2xl text-center mb-6 text-gray-300">What are your reading goals for the year?</h2>
+            <div className="flex flex-col md:flex-row gap-7">
+                <div className="flex flex-col w-full md:w-1/3 gap-2 items-center">
+                    <label className="text-gray-700 flex items-center" >
+                        📚
+                        Daily reading (min)
+                    </label>
+                    <input
+                        type="number"
+                        placeholder="10 minutes"
+                        className="h-12 p-4 rounded-3xl  border-gray-300 bg-gray-50 focus:outline-none font-light placeholder-slate-300"
+                        value={formData.readingGoals.dailyReading}
+                        onChange={(event) =>
+                            setFormData({
+                                ...formData,
+                                readingGoals: {
+                                    ...formData.readingGoals,
+                                    dailyReading: event.target.value
+                                }
+                            })
+                        }
+                    />
+
+                </div>
+                <div className="flex flex-col w-full md:w-1/3 gap-2 items-center">
+                    <label className="text-gray-700 flex items-center">
+                        📅
+                        Books per year
+                    </label>
+                    <input
+                        type="number"
+                        placeholder="10 books"
+                        className="h-12 p-4 rounded-3xl  border-gray-300 bg-gray-50 focus:outline-none font-light placeholder-slate-300"
+                        value={formData.readingGoals.booksPerYear}
+                        onChange={(event) =>
+                            setFormData({
+                                ...formData,
+                                readingGoals: {
+                                    ...formData.readingGoals,
+                                    booksPerYear: event.target.value
+                                }
+                            })
+                        }
+                    />
+                </div>
+                <div className="flex flex-col w-full md:w-1/3 gap-2 items-center">
+                    <label className="text-gray-700 flex items-center">
+                        🏷️
+                        Different genres
+                    </label>
+                    <input
+                        type="number"
+                        placeholder="5 different genres"
+                        className="h-12 p-4 rounded-3xl  border-gray-300 bg-gray-50 focus:outline-none font-light placeholder-slate-300"
+                        value={formData.readingGoals.diffGenres}
+                        onChange={(event) =>
+                            setFormData({
+                                ...formData,
+                                readingGoals: {
+                                    ...formData.readingGoals,
+                                    diffGenres: event.target.value
+                                }
+                            })
+                        }
+                    />
                 </div>
             </div>
+        </div>
     )
 }
 
--- src/components/StepsInterests.jsx
+++ src/components/StepsInterests.jsx
@@ -1,13 +1,48 @@
-"use client";
-
-import { Badge } from "flowbite-react";
 import { useState } from "react";
 import { HiCheck } from "react-icons/hi";
 
-function StepsInterest() {
+function StepsInterest({ formData, setFormData }) {
+  const [selectedGenres, setSelectedGenres] = useState([]);
+
+  const toggleGenre = (genre) => {
+    const updatedGenres = selectedGenres.includes(genre)
+      ? selectedGenres.filter((g) => g !== genre)
+      : [...selectedGenres, genre];
+
+    setSelectedGenres(updatedGenres);
+
+    setFormData({
+      ...formData,
+      genres: updatedGenres // Aquí usamos la versión actualizada de selectedGenres
+    });
+  };
+
+  const genres = [
+    "romance", "fantasy", "adventures", "drama"
+  ];
 
   return (
-    <div>e</div>
+    <div>
+      {genres.map((genre) => (
+        <button
+          key={genre}
+          onClick={() => toggleGenre(genre)}
+          style={{
+            backgroundColor: selectedGenres.includes(genre) ? "#007bff" : "transparent",
+            color: selectedGenres.includes(genre) ? "#fff" : "#000",
+            border: "1px solid #ccc",
+            borderRadius: "5px",
+            padding: "5px 10px",
+            margin: "5px",
+            cursor: "pointer",
+          }}
+          value={genre}
+        >
+          {genre}
+          {selectedGenres.includes(genre) }
+        </button>
+      ))}
+    </div>
   );
 }
 
