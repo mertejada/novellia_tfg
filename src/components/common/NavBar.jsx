85cca70f1ee331bf26571f5f56fe84b8eb26bd6d
Mercedes Tejada <mercedestejadaporcel@gmail.com>
2024-04-22 22:59:42 +0200

    gonna create  a media queries context (TEST)

--- src/components/common/NavBar.jsx
+++ src/components/common/NavBar.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import logoImg from '../../assets/img/logo.png';
+import logoImgMobile from '../../assets/img/logo-mobile.png';
+
 import { useNavigate, Link } from "react-router-dom";
 import { useAuth } from '../../contexts/AuthContext';
 
@@ -9,10 +11,27 @@ import LogoutIcon from '@mui/icons-material/Logout';
 
 const NavBar = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [img, setImg] = useState(logoImg); // Selecciona la imagen a mostrar [logoImg, logoImgMobile
     const menuRef = useRef(null);
     const navigate = useNavigate();
 
-    const { user, isAdmin, logout} = useAuth(); // Utiliza el contexto de autenticación
+    const { user, isAdmin, logout } = useAuth(); // Utiliza el contexto de autenticación
+
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth < 768) {
+                setImg(logoImgMobile);
+            } else {
+                setImg(logoImg);
+            }
+        };
+
+        window.addEventListener("resize", handleResize);
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }
+);
 
     useEffect(() => {
         const handleClickOutside = (event) => {
@@ -31,13 +50,33 @@ const NavBar = () => {
         setIsMenuOpen(!isMenuOpen);
     };
 
-    const handleLogout = () => { 
+    const handleLogout = () => {
         logout();
         navigate("/");
     };
 
+    const userLinks = [
+        { name: "Home", path: "/home" },
+        { name: "Library", path: "/bookshelf" },
+        { name: "Tracker", path: "/tracker" },
+        { name: "Discover", path: "/discover" },
+    ];
+
+    const adminLinks = [
+        { name: "Admin", path: "/admin" },
+    ];
+
+    const renderLinks = (links) => (
+        <div className="flex items-center gap-10 font-semibold">
+            {links.map((link) => (
+                <Link key={link.name} to={link.path} className=" cursor-pointer">{link.name}</Link>
+            ))}
+        </div>
+    );
+
     const renderAdminNav = () => (
-        <div className="flex items-center gap-2 text-gray-500 cursor-pointer">
+        <div className="flex items-center gap-2 text-gray-800 cursor-pointer">
+            {renderLinks(userLinks)}
             <span onClick={handleLogout}>Log out</span>
             <LogoutIcon />
         </div>
@@ -45,46 +84,43 @@ const NavBar = () => {
 
     const renderUserNav = () => (
         <>
-        <div className="flex items-center gap-10 font-semibold mr-20">
-
-        <Link to="/home" className=" cursor-pointer">Home</Link>
-        <Link to="/bookshelf" className=" cursor-pointer">Library</Link>
-        <Link to="/tracker" className=" cursor-pointer">Tracker</Link>
-        <Link to="/discover" className=" cursor-pointer">Discover</Link>
-        </div>
-        <div className="relative" ref={menuRef}>
-            {user.photoURL ? (
-                <img
-                    src={user.photoURL}
-                    className="h-8 w-8 rounded-full cursor-pointer"
-                    alt="User"
-                    onClick={toggleMenu}
-                />
-            ) : (
-                <AccountCircleIcon className="cursor-pointer" onClick={toggleMenu} />
-            )}
-            {isMenuOpen && (
-                <div className="absolute top-10 right-0 w-40 bg-white border border-gray-200 rounded shadow-md">
-                    <ul className="py-2">
-                        <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer" onClick={toggleMenu}>
-                            <SettingsIcon className="mr-2" />
-                            <Link to="/profile">Settings</Link>
-                        </li>
-                        <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer" onClick={handleLogout}>
-                            <LogoutIcon className="mr-2" />
-                            Log out
-                        </li>
-                    </ul>
-                </div>
-            )}
-        </div></>
+            <div className="flex items-center gap-2 text-gray-800 cursor-pointer">
+                {renderLinks(userLinks)}
+            </div>
+            <div ref={menuRef}>
+                {user.photoURL ? (
+                    <img
+                        src={user.photoURL}
+                        className="h-8 w-8 rounded-full cursor-pointer"
+                        alt="User"
+                        onClick={toggleMenu}
+                    />
+                ) : (
+                    <AccountCircleIcon className="cursor-pointer" onClick={toggleMenu} />
+                )}
+                {isMenuOpen && (
+                    <div className="absolute top-10 right-0 w-40 bg-white border border-gray-200 rounded shadow-md">
+                        <ul className="py-2">
+                            <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer" onClick={toggleMenu}>
+                                <SettingsIcon className="mr-2" />
+                                <Link to="/profile">Settings</Link>
+                            </li>
+                            <li className="px-4 py-2 hover:bg-gray-100 cursor-pointer" onClick={handleLogout}>
+                                <LogoutIcon className="mr-2" />
+                                Log out
+                            </li>
+                        </ul>
+                    </div>
+                )}
+            </div></>
     );
+
 
     return (
         <nav className="bg-white sticky top-0 border-b border-gray-200 z-20">
             <div className="shadow-lg flex justify-between items-center p-6 pl-8 pr-8">
                 <a href="/" className="flex items-center space-x-2">
-                    <img src={logoImg} className="h-8" alt="Novellia Logo" />
+                    <img src= {img} alt="Logo" className="h-8" />
                 </a>
                 {user ? (isAdmin ? renderAdminNav() : renderUserNav()) : null}
             </div>
---
+++ src/contexts/MediaQueries.jsx
@@ -0,0 +1,5 @@
+//voy a crear un contexto que maneje media queries
+import React, { createContext, useContext, useEffect, useState } from 'react';
+
+const MediaQueriesContext = createContext();
+
