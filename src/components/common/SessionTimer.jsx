c81a5ade430327e96491a4641cde26d42a77bba0
Mercedes Tejada <mercedestejadaporcel@gmail.com>
2024-05-21 11:49:26 +0200

    session timer restart

--- src/components/common/SessionTimer.jsx
+++ src/components/common/SessionTimer.jsx
@@ -6,7 +6,9 @@ import { doc, setDoc, updateDoc, getDoc } from "firebase/firestore";
 
 import CloseIcon from '@mui/icons-material/Close';
 import TimerRoundedIcon from '@mui/icons-material/TimerRounded';
+import RestartAltRoundedIcon from '@mui/icons-material/RestartAltRounded';
 import Alert from '@mui/material/Alert';
+import { set } from "firebase/database";
 
 
 const SessionTimer = ({ setShowSessionTimer }) => {
@@ -40,6 +42,17 @@ const SessionTimer = ({ setShowSessionTimer }) => {
     }
 
     const handleStop = async () => {
+        if(time == 0) {
+            setMessage({ type: "error", content: "Session time is 0" });
+
+            setTimeout(() => {
+                setMessage({ type: null, content: null });
+            }
+            , 3500);
+            return;
+        }
+
+
         if (timerId) {
             clearInterval(timerId);
             setTimerId(null);
@@ -69,12 +82,20 @@ const SessionTimer = ({ setShowSessionTimer }) => {
         }
 
         setMessage({ type: "success", content: "Session saved" });
-        
+
         setTimeout(() => {
             setShowSessionTimer(false);
-        }, 4000);
+        }, 3000);
 
         console.log("Session saved");
+    }
+
+    const handleRestart = () => {
+        setTime(0);
+        if (timerId) {
+            clearInterval(timerId);
+            setTimerId(null);
+        }
     }
 
     useEffect(() => {
@@ -94,50 +115,57 @@ const SessionTimer = ({ setShowSessionTimer }) => {
 
     return (
         <div className="fixed top-0 left-0 w-full h-full bg-gray-900 bg-opacity-50 flex justify-center items-center z-50">
-            <div className="bg-white p-4 rounded-lg">
+            <div className="bg-white xs:p-2 sm:p-4 rounded-lg">
                 <div className="flex justify-end items-center gap-1 text-gray-300 hover:text-gray-500 transition-all duration-150 ease-in-out">
-                    <CloseIcon className="cursor-pointer" onClick={() => setShowSessionTimer(false)} fontSize="small" />
+                    <CloseIcon className="cursor-pointer m-2" onClick={() => setShowSessionTimer(false)} fontSize="small" />
 
                 </div>
                 <div className="flex flex-col justify-center items-center gap-2 text-center p-6">
                     <div className="flex flex-col items-center gap-1">
-                    <TimerRoundedIcon />
-                    <h2 className="text-xl">Reading session</h2>
-                    <p className="text-gradient gradient text-sm">{today.toDateString()}</p>
+                        <TimerRoundedIcon />
+                        <h2 className="text-xl">Reading session</h2>
+                        <p className="text-gradient gradient text-sm">{today.toDateString()}</p>
                     </div>
-                    
+
                     <p className=" text-6xl font-extralight font-poppins">{formatTime(time)}</p>
 
 
-                    <div className="flex justify-center items-center mt-5 ">
-                        <button className="flex gap-1 font-normal  px-2 py-1 items-center  transition-all duration-150 ease-in-out text-gray-300  hover:text-red-600 hover:bg-gray-100 hover:border-gray-100  rounded-full  border-gray-300" onClick={handleStop}>
+                    <div className="flex  items-center xs:pl-1 sm:pl-3 mt-5 gap-1   ">
+                        
+                    <button className="flex font-normal px-2 py-1 items-center justify-center  transition-all duration-150 ease-in-out text-gray-300  hover:text-crayola hover:bg-gray-100 hover:border-gray-100  rounded-full  border-gray-300" onClick={() => handleRestart()}>
+                            <RestartAltRoundedIcon />
+                        </button>
+                        <button className="flex gap-1 font-normal px-2 py-1 items-center justify-center  transition-all duration-150 ease-in-out text-gray-300  hover:text-red-600 hover:bg-gray-100 hover:border-gray-100  rounded-full  border-gray-300" onClick={handleStop}>
                             <StopCircle /> Save
                         </button>
                         {!timerId ?
 
                             (
-                                <button className="flex gap-1 font-normal  px-2 py-1 items-center  transition-all duration-150 ease-in-out hover:bg-black hover:text-white hover:border-black  rounded-full  border-black text-black" onClick={handlePlay}>
+                                <button className="flex gap-1 font-normal w-28    px-4 py-1 items-center transition-all duration-150 ease-in-out hover:bg-black hover:text-white hover:border-black  rounded-full  border-black text-black" onClick={handlePlay}>
                                     <PlayCircle className="" /> Start
                                 </button>
                             )
                             :
 
                             (
-                                <button className="flex gap-1 font-normal  px-2 py-1 items-center  transition-all duration-150 ease-in-out  hover:bg-black hover:text-white hover:border-black  rounded-full  border-black text-black" onClick={handlePause}>
+                                <button className="flex gap-2 font-normal w-28   px-4 py-1 items-center   transition-all duration-150 ease-in-out  hover:bg-black hover:text-white hover:border-black  rounded-full  border-black text-black" onClick={handlePause}>
                                     <PauseCircle /> Pause
                                 </button>)
                         }
+                        
+                        
+                        
 
 
 
                     </div>
 
-                    
-                    {message.type === "success" ?
-                        <Alert severity="success" className="mt-2">{message.content}</Alert>
-                        :
-                        <p className="text-xs bg-slate-100 text-slate-600 w-48 mt-5 p-3 rounded-xl">Always save your session before closing the window</p>
+
+                    {message.type !== null ? <Alert severity={message.type} className="mt-5">{message.content}</Alert> :
+                    <p className="text-xs bg-slate-100 text-slate-600 w-48 mt-5 p-3 rounded-xl">Always save your session before closing the window</p>
                     }
+
+                    
                 </div>
             </div>
         </div>
